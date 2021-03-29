21b7f4788ffe5b461a9d7412d3315f148105ba7c
Sai Shanmukha Narumanchi <nss10@outlook.com>
2021-03-28 22:22:20 -0500

    Added an interface to Get and Set Actions from textbox

--- client/index.js
+++ client/index.js
@@ -1,5 +1,9 @@
-let bulletList = list = document.getElementById("bulletList");
+let bulletList = document.getElementById("bulletList");
+let tbLocal = document.getElementById("local-actions");
+let tbRemote = document.getElementById("incoming-actions");
 let viewList = [];
+localActionList = [];
+remoteActionList=[];
 
 function renderRows(){
     bulletList.innerHTML="";
@@ -25,14 +29,57 @@ function addRow(rowId){
         rowId++;
         viewList.splice(rowId, 0 , text);
         renderRows();
-        console.log("Add("+rowId+ ","+text+")")
+        actionText = "Add ( "+rowId+ " , "+text+" ) ";
+        localActionList.push({"action" : "Add", "index" : rowId, "text" : text});
+        tbLocal.value += actionText + "\n";
+        console.log(actionText);
+        
     }
 }
 
 function delRow(rowId){
     viewList.splice(rowId, 1);
     renderRows();
-    console.log("Delete("+rowId+ ")")
+    actionText = "Delete ( "+rowId+ " )";
+    localActionList.push({"action" : "Delete", "index" : rowId});
+    tbLocal.value += actionText + "\n";
+    console.log(actionText);
+}
+
+function merge(){
+    remoteActionList = [];
+    incomingList = tbRemote.value.split("\n");
+    for(let actionIndex in incomingList){
+        if(!incomingList[actionIndex]){
+            continue;
+        }
+        actionObject = getActionObjectFromText(incomingList[actionIndex]);
+        remoteActionList.push(actionObject);
+        if(actionObject.action == "Add"){
+            viewList.splice(actionObject.index, 0, actionObject.text);
+        }
+        else if(actionObject.action == "Delete"){
+            viewList.splice(actionObject.index, 1);
+        }
+        else{
+            alert("Couldn't merge : Formatting error at "+ incomingList[actionIndex]);
+        }
+    }
+    tbRemote.value="";
+    console.log(remoteActionList);
+    renderRows();
+}
+
+function getActionObjectFromText(text){
+    let splitList = text.split(" ");
+    let actionObject =  {
+            "action": splitList[0], 
+            "index" : splitList[2],
+        }
+    if(actionObject.action=="Add"){
+        actionObject.text = splitList[4];
+    }
+    return actionObject;
 }
 
 renderRows();
